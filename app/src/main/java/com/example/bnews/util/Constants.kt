78bbd1d1079f638bbbe002901b4fdf6453851b2c
Roland Ruales <bokz.bokz.02@gmail.com>
2023-05-26 22:12:57 +0800

    Added retrofit instance, api classes

---
+++ app/src/main/java/com/example/bnews/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.bnews.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "d86978cb6c8c4b32a4240a1ef7f61a78"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
